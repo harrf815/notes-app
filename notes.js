eb61ec6aa9591978645a1bd22a98fe00bfceb850
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-01 16:56:58 -0700

    delete notes

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function () {
     return 'Your notes...'
@@ -8,7 +9,7 @@ const getNotes = function () {
 //! Adding a note 
 const addNote = function (title, body) {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter( function (note) {
+    const duplicateNotes = notes.filter(function(note) {
         return note.title === title 
     })
 
@@ -18,17 +19,27 @@ const addNote = function (title, body) {
             body: body 
         })
         saveNotes(notes)
-        console.log('New Note added!')
+        console.log(chalk.green.inverse('New Note added!'))
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
 
-    
 }
 
 //! Deleting a Note 
 const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function(notes){
+        return note.title !== title 
+    })
 
+    if(notes.length > notesToKeep.length){
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No Note found!'))
+    }
+    
 }
 
 //! Storing the data in JSON 
